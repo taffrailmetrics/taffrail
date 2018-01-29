b31d0f7336b81899c0cca0cb6f1d03bbcc41caa4
yaron2 <schneider.yaron@live.com>
2018-01-29 23:47:48 +0200

    updated tests

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 from kubernetes import client, config
-from taffrail_client import MetricsClient
+from taffrail.taffrail_client import MetricsClient
 
 config.load_kube_config()
 conf = client.Configuration()
@@ -9,7 +9,7 @@ names = metrics_client.get_sources()
 r1 = metrics_client.get_metrics_with_source('metrics-server')
 r2 = metrics_client.get_metrics_with_source('metrics-api')
 r3 = metrics_client.get_metrics_with_source('kube-state-metrics')
-response = metrics_client.get_metrics()
-print("lala")
+all = metrics_client.get_metrics()
+print("done")
 
 
